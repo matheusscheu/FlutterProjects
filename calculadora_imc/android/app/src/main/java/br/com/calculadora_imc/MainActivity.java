1a21bcbb52361e7928e1c2c2701676cdda5dac8a
Matheus Scheunemann <matheuss.sls@gmail.com>
2021-01-06 16:12:31 -0300

    Adicionando projetos ja finalizados

---
+++ calculadora_imc/android/app/src/main/java/br/com/calculadora_imc/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.calculadora_imc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ listadetarefas/android/app/src/main/java/br/com/listadetarefas/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.listadetarefas;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
